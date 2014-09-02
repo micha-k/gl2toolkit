06c9b384d5f8a40c56f70ae2911cf07157144f41
Michael Kessel <kessel@Michaels-MacBook-Pro.local>
2014-09-02 20:06:13 +0200

    Added debug mode (from env)

---
+++ lib/var.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# 
+# Copyright (c) 2014 Michael Kessel
+# 
+# This file is part of gl2toolkit
+#
+# This program is free software under the terms of the MIT License 
+# You should have received a copy of the MIT License
+# along with this program; if not, see <http://opensource.org/licenses/MIT>.
+#
+
+
+def debug():
+    return os.getenv('DEBUG', false)
